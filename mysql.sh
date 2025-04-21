40368de8ee1b91aa292484a5c7a7dc6fab69b430
daws2025 <devsecopswithaws@gmail.com>
2025-04-21 16:09:55 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -16,9 +16,9 @@ mkdir -p /var/log/project-logs
 VALIDATE()
 if [ $1 -ne 0 ]
 then
-    echo "$2 is $R FAILURE $N"
+    echo -e "$2 is $R FAILURE $N"
 else
-    echo "$2 is $G SUCCESS $N"
+    echo -e "$2 is $G SUCCESS $N"
 fi
 
 CHECK_ROOT()
